914fcdbc290a23e2668b7edad47a510dd299c4e4
pjaithwar <palash.jaithwar@gmail.com>
2021-06-28 21:10:00 +0530

    added receive email feature

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -14,7 +14,7 @@ const HomePage =()=>{
         <section className="text-gray-600 body-font">
   <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
     <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center" style={{paddingTop:80}}>
-      <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900" style={{fontFamily:'"Times New Roman"', fontSize:45}}>I'm Palash Jaithwar
+      <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900" style={{fontFamily:'"Times New Roman"', fontSize:45}}>Palash Jaithwar
       </h1>
       <p className="mb-8 leading-relaxed" style={{fontSize:20, color:"black", fontWeight:"bold"}}>Hi There!</p>
       <p className="mb-8 leading-relaxed">I'm Palash Jaithwar a MERN Stack Developer, Cross-platform Mobile Developer and also a Guitarist.</p>
