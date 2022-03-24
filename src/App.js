fdd8ec4a717cb7186bf591fc86925bbf28771bb3
pjaithwar <palash.jaithwar@gmail.com>
2022-03-25 00:21:21 +0530

    fixed some errors

--- src/App.js
+++ src/App.js
@@ -1,35 +1,33 @@
-import './App.css';
-import NavBar from './Components/NavBar'; 
-import AboutMe from './Components/AboutMe';
-import Experience from './Components/Experience';
-import Skills from './Components/Skills';
-import ContactMe from './Components/ContactMe';
-import HomePage from './Components/HomePage';
-import Portfolio from './Components/Portfolio';
-import {BrowserRouter as Router , Switch, Route } from 'react-router-dom';
+import "./App.css";
+import NavBar from "./Components/NavBar";
+import AboutMe from "./Components/AboutMe";
+import Experience from "./Components/Experience";
+import Skills from "./Components/Skills";
+import ContactMe from "./Components/ContactMe";
+import HomePage from "./Components/HomePage";
+import Portfolio from "./Components/Portfolio";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <Router>
-    <div className="App">
-      <NavBar/>
-      {/* <HomePage/>
+      <div className="App">
+        <NavBar />
+        {/* <HomePage/>
       
       <AboutMe/>
       <Skills/>
       <Experience/>
       <ContactMe/> */}
-      <Switch>
-      
-        <Route path="/"  exact component={HomePage}/>
-        <Route path="/about"  exact component={AboutMe}/>
-        <Route path="/skills"  exact component={Skills}/>
-        <Route path="/experience"  exact component={Experience}/>
-        <Route path="/contact"  exact component={ContactMe}/>
-        <Route path="/portfolio"  exact component={Portfolio}/>
-      </Switch>
-
-    </div>
+        <Switch>
+          <Route path="/" exact component={HomePage} />
+          <Route path="/about" component={AboutMe} />
+          <Route path="/skills" component={Skills} />
+          <Route path="/experience" component={Experience} />
+          <Route path="/contact" component={ContactMe} />
+          <Route path="/portfolio" component={Portfolio} />
+        </Switch>
+      </div>
     </Router>
   );
 }
